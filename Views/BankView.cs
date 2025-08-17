0c2745ae79ab5377610c18f23494f4e78effb939
Nodira Kazakova <kazakovanodira04@gmail.com>
2025-08-16 20:45:27 -0400

    Added BankView

---
+++ Views/BankView.cs
@@ -0,0 +1,14 @@
+using BankLevel1.Models;
+
+namespace BankLevel1.Views;
+
+public class BankView
+{
+    private readonly Bank _bank;
+
+    public BankView(Bank bank)
+    {
+        _bank = bank;
+    }
+    
+}
