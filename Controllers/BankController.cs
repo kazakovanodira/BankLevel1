af271db364db075e8bffd9e0b3b4995ae71a9564
Nodira Kazakova <kazakovanodira04@gmail.com>
2025-08-16 20:34:14 -0400

    Create Account method added

--- Controllers/BankController.cs
+++ Controllers/BankController.cs
@@ -1,8 +1,9 @@
+using BankLevel1.Interfaces;
 using BankLevel1.Models;
 
 namespace BankLevel1.Controllers;
 
-public class BankController
+public class BankController : IBankController
 {
     private readonly Bank _bank;
 
@@ -10,6 +11,23 @@ public class BankController
     {
         _bank = bank;
     }
-    
-    
+
+    public Guid CreateAccount(string name)
+    {
+        Account newAccount = new Account
+        {
+            Name = name,
+            AccountNumber = Guid.NewGuid(),
+            Balance = 0
+        };
+        
+        _bank.Accounts.Add(newAccount.AccountNumber, new AccountController(newAccount));
+
+        return newAccount.AccountNumber;
+    }
+
+    public IAccountController? GetAccount(Guid accountNumber)
+    {
+        throw new NotImplementedException();
+    }
 }
